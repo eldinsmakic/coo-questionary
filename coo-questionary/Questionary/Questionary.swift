47bcad40509f3f9422fcd37d545c1e6d7685b946
Smakic Eldin <eldin.smakic.etu@univ-lille.fr>
2020-03-11 21:06:48 +0100

    [Feat] : add Questionary Class

---
+++ coo-questionary/Questionary/Questionary.swift
@@ -0,0 +1,41 @@
+//
+//  Questionary.swift
+//  coo-questionary
+//
+//  Created by eldin smakic on 11/03/2020.
+//  Copyright © 2020 eldin smakic. All rights reserved.
+//
+
+import Foundation
+
+/// Questionary represent  a list of question
+class Questionary {
+    private var questions : [Question]
+    private var score : Int
+    
+    init()
+    {
+        self.questions = [Question]()
+        self.score = 0
+    }
+    
+    
+    /// Return all questions
+    func getAllQuestions() -> [Question]
+    {
+        return self.questions
+    }
+    
+    /// add a qestion to the questionary
+    func addQuestion(question : Question)
+    {
+        self.questions.append(question)
+    }
+    
+    /// return number of question in questionary
+    func getNbQuestion() -> Int
+    {
+        return self.questions.count
+    }
+    
+}
